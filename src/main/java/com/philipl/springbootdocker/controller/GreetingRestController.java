b454b3c7024cb20f8a1ffe9713b33cff877c9317
philipl <philipl@synnex.com>
2018-04-14 15:28:33 +0800

    add log4j dependency

--- src/main/java/com/philipl/springbootdocker/controller/GreetingRestController.java
+++ src/main/java/com/philipl/springbootdocker/controller/GreetingRestController.java
@@ -1,16 +1,21 @@
 package com.philipl.springbootdocker.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Calendar;
+import java.util.Date;
 
 @RestController
 public class GreetingRestController {
 
+    private Logger logger = Logger.getLogger(GreetingRestController.class);
+
     @RequestMapping("/hello")
     public String hello(){
-        System.out.println("hello " + Calendar.getInstance());
-        return "<h1>Hello world!</h1>";
+        String timeString = String.format("%tT%n", new Date());
+        logger.info("hello: "+ timeString);
+        return "<h1>Hello world, </h1>"+timeString;
     }
 }
